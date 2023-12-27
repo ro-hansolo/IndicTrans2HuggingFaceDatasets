047c8fa943408a0b7411415923ab12537557af0e
Rohan Shiralkar <rohan@bhaiyapoints.in>
2023-12-27 13:49:54 +0530

    Fix Batching and Loading from checkpoints

--- translate.py
+++ translate.py
@@ -15,7 +15,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 logger = logging.getLogger(__name__)
 # Constants
 en_indic_ckpt_dir = "ai4bharat/indictrans2-en-indic-dist-200M"
-BATCH_SIZE = 256
+BATCH_SIZE = 128
 DEVICE = "cuda"
 TARGET_LANGUAGES = ["hin_Deva","tam_Taml","mar_Deva","mal_Mlym","kan_Knda"]  # Hindi, Tamil, Malayalam, Marathi, Kannada
 NUM_SAMPLES = 12000
@@ -49,37 +49,40 @@ def batch_translate(input_sentences, src_lang, tgt_lang, model, tokenizer, initi
     pbar = tqdm(total=len(input_sentences), desc="Translating")
 
     while i < len(input_sentences):
-        try:
-            batch = input_sentences[i: i + current_batch_size]
+        current_batch_size = initial_batch_size
+        while True:
+            try:
+                batch = input_sentences[i: i + current_batch_size]
 
-            # Preprocess the batch and extract entity mappings
-            batch, entity_map = preprocess_batch(batch, src_lang=src_lang, tgt_lang=tgt_lang)
+                # Preprocess the batch and extract entity mappings
+                batch, entity_map = preprocess_batch(batch, src_lang=src_lang, tgt_lang=tgt_lang)
 
-            # Tokenize the batch and generate input encodings
-            inputs = tokenizer(batch, src=True, truncation=True, padding="longest", return_tensors="pt", return_attention_mask=True).to(DEVICE)
+                # Tokenize the batch and generate input encodings
+                inputs = tokenizer(batch, src=True, truncation=True, padding="longest", return_tensors="pt", return_attention_mask=True).to(DEVICE)
 
-            # Generate translations using the model
-            with torch.no_grad():
-                generated_tokens = model.generate(**inputs, use_cache=True, min_length=0, max_length=256, num_beams=5, num_return_sequences=1)
+                # Generate translations using the model
+                with torch.no_grad():
+                    generated_tokens = model.generate(**inputs, use_cache=True, min_length=0, max_length=256, num_beams=5, num_return_sequences=1)
 
-            # Decode the generated tokens into text
-            generated_tokens = tokenizer.batch_decode(generated_tokens.detach().cpu().tolist(), src=False)
+                # Decode the generated tokens into text
+                generated_tokens = tokenizer.batch_decode(generated_tokens.detach().cpu().tolist(), src=False)
 
-            # Postprocess the translations, including entity replacement
-            translations += postprocess_batch(generated_tokens, lang=tgt_lang, placeholder_entity_map=entity_map)
+                # Postprocess the translations, including entity replacement
+                translations += postprocess_batch(generated_tokens, lang=tgt_lang, placeholder_entity_map=entity_map)
 
-            del inputs
-            torch.cuda.empty_cache()
-
-            pbar.update(current_batch_size)
-            i += current_batch_size  # Move to the next batch
-        except RuntimeError as e:
-            if 'out of memory' in str(e) and current_batch_size > 1:
-                logger.warning(f"OOM error caught during translation, reducing batch size to {current_batch_size // 2}")
-                current_batch_size //= 2  # Reduce the batch size
+                del inputs
                 torch.cuda.empty_cache()
-            else:
-                raise e  # If not OOM, or batch size is 1, re-raise the exception
+
+                pbar.update(current_batch_size)
+                i += current_batch_size  # Move to the next batch
+                break
+            except RuntimeError as e:
+                if 'out of memory' in str(e) and current_batch_size > 1:
+                    logger.warning(f"OOM error caught during translation, reducing batch size to {current_batch_size // 2}")
+                    current_batch_size //= 2  # Reduce the batch size
+                    torch.cuda.empty_cache()
+                else:
+                    raise e  # If not OOM, or batch size is 1, re-raise the exception
 
     pbar.close()
     return translations
@@ -126,8 +129,11 @@ def load_checkpoint(checkpoint_dir=CHECKPOINT_DIR):
             metadata = json.load(f)
         data_file = os.path.join(checkpoint_dir, f"{metadata['current_lang']}_data.pkl")
         data = pd.read_pickle(data_file)
+        if isinstance(data, pd.DataFrame):
+            data = data.to_dict(orient='records')
         return data, metadata
     return None, None
+
 
 def main():
     logger.info("Loading UltraChat 200k dataset...")
@@ -135,6 +141,8 @@ def main():
     
     # Select and shuffle samples
     checkpoint_data, checkpoint_metadata = load_checkpoint()
+    # print("Checkpoint data sample:", checkpoint_data.tail())
+    start_lang_index = 0
     if checkpoint_data is not None:
         logger.info(f"Resuming from checkpoint: {checkpoint_metadata['current_lang']}")
         selected_samples = checkpoint_data
@@ -146,6 +154,7 @@ def main():
     # Split into language groups
     group_size = NUM_SAMPLES // (len(TARGET_LANGUAGES) + 1)  # +1 for English
     groups = [selected_samples[i:i + group_size] for i in range(0, NUM_SAMPLES, group_size)]
+
     logger.info(f"Split dataset into {len(groups)} groups of size {group_size}")
 
     # Initialize translation model and tokenizer
@@ -159,18 +168,20 @@ def main():
         # Replace original sentences with translated ones
         for sample in groups[TARGET_LANGUAGES.index(lang)]:
             sample['lang'] = lang
+            print(sample)
             for message in sample['messages']:
                 if message['role'] != 'system':
                     translated_content = ' '.join([translations.get(sentence, sentence) for sentence in re.split(r'(?<=[.!?;:])\s+', message['content'])])
                     message['content'] = translated_content
         # Save checkpoint
-            checkpoint_metadata = {"current_lang": lang}
-            save_checkpoint(sum(groups, []), checkpoint_metadata)
+        checkpoint_metadata = {"current_lang": lang}
+        save_checkpoint(sum(groups, []), checkpoint_metadata)
 
     # Combine all groups into a single dataset
     final_samples = sum(groups, [])
     final_dataset = Dataset.from_pandas(pd.DataFrame(final_samples))
     final_dataset.save_to_disk("ultrachat_multilingual")
+    final_dataset.push_to_hub("rohansolo/BB-Ultrachat-IndicLingual6-12k")
 
 if __name__ == "__main__":
     main()
